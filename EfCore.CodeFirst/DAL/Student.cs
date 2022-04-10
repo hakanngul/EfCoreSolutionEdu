de47ba2fca585b284227baf298645039005db416
Hakan GUL <hakanngul@icloud.com>
2022-04-10 20:20:22 +0300

    many-to-many

---
+++ EfCore.CodeFirst/DAL/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace EfCore.CodeFirst.DAL;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public List<Teacher> Teachers { get; set; }
+}
---
+++ EfCore.CodeFirst/DAL/Teacher.cs
@@ -0,0 +1,8 @@
+﻿namespace EfCore.CodeFirst.DAL;
+
+public class Teacher
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Student> Students { get; set; }
+}
