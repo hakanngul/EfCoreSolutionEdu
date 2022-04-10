9fbdede3ede27baedc9a34c85b697facf082af79
Hakan GUL <hakanngul@icloud.com>
2022-04-10 12:06:42 +0300

    ggg

--- EfCore.CodeFirst/Program.cs
+++ EfCore.CodeFirst/Program.cs
@@ -7,18 +7,5 @@ using Microsoft.EntityFrameworkCore;
 Initializer.Build();
 using (var context = new AppDbContext())
 {
-    string[] pr = {"Kalem", "Defter", "Kitap", "Çanta", "Boya Çantası", "Resim Defteri", "Uhu", "Makas"};
-
-    for (var i = 0; i < 5000; i++)
-    {
-        await context.Products.AddAsync(new()
-        {
-            Name = pr.PickRandom()+ " " + Random.Shared.Next(i, i + 2513),
-            Price = Random.Shared.Next(0, i + 1000),
-            Stock = Random.Shared.Next(0, i + 3410),
-            DiscountPrice = Random.Shared.Next(0, i + 9410),
-            Barcode = Random.Shared.Next(0, i + 1400)
-        });
-    }
-    await context.SaveChangesAsync();
+    
 }
