c413b60e102d4d0162c0a3e7c2d8292bd4b372ee
Hakan GUL <hakanngul@icloud.com>
2022-04-24 19:14:52 +0300

    Explicit Loading

--- EfCore.CodeFirst/Program.cs
+++ EfCore.CodeFirst/Program.cs
@@ -7,46 +7,25 @@ using Microsoft.EntityFrameworkCore;
 Initializer.Build();
 using (var context = new AppDbContext())
 {
-    // var category = new Category()
-    // {
-    //     Name = "Kalemler"
-    // };
-    // var category = context.Categories.First();
-    //
-    // category.Products.Add(new Product() {Name = "Kalem1", Price = 100, Stock = 1200, Barcode = 456, ProductFeature = new ProductFeature()
-    // {
-    //     Color = "Red",Height = 100,Width = 200
-    // }});
-    // category.Products.Add(new Product() {Name = "Kalem2", Price = 1500, Stock = 2500, Barcode = 789, ProductFeature = new ProductFeature()
-    // {
-    //     Color = "Blue",Height = 100,Width = 200
-    // }});
-    // category.Products.Add(new Product() {Name = "Kalem3", Price = 400, Stock =3300, Barcode = 567, ProductFeature = new ProductFeature()
-    // {
-    //     Color = "Green",Height = 100,Width = 200
-    // }});
-    // await context.AddAsync(category);
+    //Expilict Loading işleminde bağlı olan product veya diğer bağlı entrylerin yüklenme işlemini sonra yapabiliyoruz.
+    // var category = context.Categories.FirstOrDefault();
 
-
-    //Eager Loading Categoriyi çekerken buna bağlı productlarıda çekmek için kullanıyoruz ve aşşağıdaki şekilde
-    //Eager Loading JS deki then işleminide ifade etmektedir.
-    //İç içe geçmiş bir yapıyı yani Entityden entitye sıçrama yapabiliyoruz
-    // var categoryWithProducts = context.Categories.Include(x => x.Products)
-    //     .ThenInclude(x => x.ProductFeature).First();
-    //
-    // categoryWithProducts.Products.ForEach(product =>
+    // if (true)
     // {
-    //     Console.WriteLine($"{categoryWithProducts.Name} => {product.Name} => {product.ProductFeature.Color}");
-    //     
-    // });
-    //Categoryden productFeature a gittik bu sefer de productFeature dan categorye gideibliyoruz
-    // var productFeature = context.ProductFeatures.Include(x => x.Product)
-    //     .ThenInclude(x => x.Category).First();
+        // Product ile category arasında collection ilişki bulunuyor.
+        // var products = context.Products.Where(x => x.CategoryId == category.Id);
+        // burada if doğru olduğunda category e producları yükleme yapıyoruz.
+        // context.Entry(category).Collection(x => x!.Products).Load();
+        // category?.Products.ForEach(x => { Console.WriteLine(x.Name); });
+    // }
+    var product = context.Products.FirstOrDefault();
+    if (true)
+    {
+        // context.ProductFeatures.Where(x => x.Id == product.Id).First();
+        //Product ile ProductFeature arasında referans üzerinden erişim yapabiliyoruz.
+        //çünkü referans ilişkisi bulunuyor.
+        context.Entry(product).Reference(x=>x.ProductFeature).Load();
+    }
     
-    //bu sefer product üzerinden hem productFeature a hemde category e gidebiliyoruz
-    // var product = context.Products.Include(x => x.ProductFeature)
-    //     .Include(x => x.Category).First();
-
-    //await context.SaveChangesAsync();
     Console.WriteLine("İşlem Başarılı oldu!");
 }
