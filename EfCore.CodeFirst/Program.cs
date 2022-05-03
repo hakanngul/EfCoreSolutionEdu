e4ccb717123c4d123272e4b1ac9bf797719187db
Hakan GUL <hakanngul@icloud.com>
2022-05-03 18:25:07 +0300

    InnerJoin Yöntem1 => Yeni bir isimsiz class yöntemi ile verileri ele almak.

--- EfCore.CodeFirst/Program.cs
+++ EfCore.CodeFirst/Program.cs
@@ -3,31 +3,37 @@
 using EfCore.CodeFirst;
 using EfCore.CodeFirst.DAL;
 
-// Initializer.Build();
 using (var context = new AppDbContext())
 {
-    var category = new Category() {Name = "Kalemler"};
-    category.Products.Add(new()
+    // var category = new Category() {Name = "Kalemler"};
+    // category.Products.Add(new()
+    // {
+    //     Name = "Kurşun Kalem", Price = 100, Stock = 100, Barcode = 123, Url = "Kursunx.jpg",
+    //     ProductFeature = new ProductFeature() {Color = "Red", Height = 150, Width = 300}
+    // });
+    // category.Products.Add(new()
+    // {
+    //     Name = "Tükenmez Kalem", Price = 600, Stock = 230, Barcode = 567, Url = "tukenmezx.jpg",
+    //     ProductFeature = new ProductFeature() {Color = "Green", Height = 250, Width = 350}
+    // });
+    // category.Products.Add(new()
+    // {
+    //     Name = "Boya Kalemi", Price = 100, Stock = 100, Barcode = 542, Url = "boyax.jpg",
+    //     ProductFeature = new ProductFeature() {Color = "Blue", Height = 150, Width = 300}
+    // });
+    // category.Products.Add(new()
+    // {
+    //     Name = "Süs Kalemi", Price = 100, Stock = 100, Barcode = 123, Url = "susx.jpg",
+    //     ProductFeature = new ProductFeature() {Color = "Yellow", Height = 150, Width = 300}
+    // });
+    // context.Categories.Add(category);
+    // context.SaveChanges();
+
+    var result = context.Categories.Join(context.Products, x => x.Id, y => y.CategoryId, (c, p) => new
     {
-        Name = "Kurşun Kalem", Price = 100, Stock = 100, Barcode = 123, Url = "Kursunx.jpg",
-        ProductFeature = new ProductFeature() {Color = "Red", Height = 150, Width = 300}
-    });
-    category.Products.Add(new()
-    {
-        Name = "Tükenmez Kalem", Price = 600, Stock = 230, Barcode = 567, Url = "tukenmezx.jpg",
-        ProductFeature = new ProductFeature() {Color = "Green", Height = 250, Width = 350}
-    });
-    category.Products.Add(new()
-    {
-        Name = "Boya Kalemi", Price = 100, Stock = 100, Barcode = 542, Url = "boyax.jpg",
-        ProductFeature = new ProductFeature() {Color = "Blue", Height = 150, Width = 300}
-    });
-    category.Products.Add(new()
-    {
-        Name = "Süs Kalemi", Price = 100, Stock = 100, Barcode = 123, Url = "susx.jpg",
-        ProductFeature = new ProductFeature() {Color = "Yellow", Height = 150, Width = 300}
-    });
-    context.Categories.Add(category);
-    context.SaveChanges();
+        CategoryName = c.Name,
+        ProductName = p.Name,
+        ProductPrice = p.Price
+    }).ToList();
     Console.WriteLine("İşlem Başarılı oldu!");
 }
