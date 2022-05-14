35058c2bf1bfbda7b40f9862b326fc51a3391754
Hakan GUL <hakanngul@icloud.com>
2022-05-14 20:30:42 +0300

    Pagination - 2

--- EfCore.CodeFirst/Program.cs
+++ EfCore.CodeFirst/Program.cs
@@ -1,58 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using EfCore.CodeFirst;
 using EfCore.CodeFirst.DAL;
-using EfCore.CodeFirst.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Internal;
 
-List<Product> GetProducts(int page, int pageSize)
+foreach (var product in GetProducts(3, 6))
+{
+    Console.WriteLine($"{product.Id} {product.Name} {product.Price} {product.Stock}");
+}
+
+static List<Product> GetProducts(int page, int pageSize)
 {
     using (var context = new AppDbContext())
     {
-        /// <summary>
-        ///    Sayfalama işlemleri
-        /// 1 - Take(10)
-        /// 2 - Skip(2) 1 2 3 4 5 6 7 8 9 10 Skip ilk 2 sini atlar sonraki 2 taneyi alır
-        /// </summary>
-        // var category = new Category() {Name = "Defterler"};
+        //page = 1 pageSize = 3 => ilk 3 data => skip(0) take(3) (page-1)*pageSize => (1-1)*3 = 0
+        //page = 2 pageSize = 3 => ikinci 3 data => skip(3) take(3) (page-1)*pageSize => (2-1)*3 = 3
+        //page = 3 pageSize = 3 => üçüncü 3 data => skip(6) take(3) (page-1)*pageSize => (3-1)*3 = 6
 
-        #region DataInsert
-
-        // category.Products.Add(new Product()
-        // {
-        //     Name = "Defter 1", Price = 100, Stock = 7031, Barcode = 345, Url = "test1.com",
-        //     ProductFeature = new ProductFeature() {Color = "Beyaz", Height = 465, Width = 345}
-        // });
-        // category.Products.Add(new Product()
-        // {
-        //     Name = "Defter 2", Price = 1600, Stock = 5031, Barcode = 567, Url = "test2.com",
-        //     ProductFeature = new ProductFeature() {Color = "Mavi", Height = 465, Width = 345}
-        // });
-        // category.Products.Add(new Product()
-        // {
-        //     Name = "Defter 3", Price = 1800, Stock = 1531, Barcode = 124, Url = "test3.com",
-        //     ProductFeature = new ProductFeature() {Color = "Yeiş", Height = 465, Width = 345}
-        // });
-        // category.Products.Add(new Product()
-        // {
-        //     Name = "Defter 4", Price = 1100, Stock = 1531, Barcode = 876, Url = "test4.com",
-        //     ProductFeature = new ProductFeature() {Color = "Turuncu", Height = 465, Width = 345}
-        // });
-        // category.Products.Add(new Product()
-        // {
-        //     Name = "Defter 5", Price = 3100, Stock = 1031, Barcode = 567, Url = "test5.com",
-        //     ProductFeature = new ProductFeature() {Color = "Mor", Height = 465, Width = 345}
-        // });
-        //
-        // context.Add(category);
-        // context.SaveChanges();
-
-        #endregion
-
-
-        Console.WriteLine("İşlem Başarılı oldu!");
+        return context.Products
+            .OrderByDescending(x => x.Id)
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize).ToList();
     }
-
-    return null;
 }
